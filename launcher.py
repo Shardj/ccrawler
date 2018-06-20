1434567299401448fe6706c75e50ddb48927dff6
George Appleton <social@georgeappleton.co.uk>
2018-06-20 15:26:53 +0100

    removed main ini, unused, launcher no longer uses ini for pointless project dir storage

--- launcher.py
+++ launcher.py
@@ -1,14 +1,9 @@
-import subprocess, os, configparser, builtins, imp
+import subprocess, pathlib, configparser, builtins, imp
 
-# Store project absolute path
-parse = configparser.ConfigParser()
-path = './settings/main.ini'
-parse.read(path)
-parse['project']['path'] = os.path.abspath("./")
-with open(path, 'w') as configfile:
-    parse.write(configfile)
+# Store project absolute path in global __builtins__
+projectDir = pathlib.Path(__file__).parent
 
-builtins.setAbsolute(parse['project']['path']) # global variables for absolute path imports
+builtins.setAbsolute(projectDir) # global variable for absolute path imports
 builtins.projectRelativeImport = projectRelativeImport # new global function for importing using project relative path
 def projectRelativeImport(fileName, projectRelativePath, moduleName = None):
     if moduleName is None:
