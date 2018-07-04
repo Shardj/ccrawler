59d2c5f9713586fb7941db2f02a0ce33506a142f
George Appleton <social@georgeappleton.co.uk>
2018-07-04 14:31:48 +0100

    purging invalid symbols from paths and moved around some whitespace purges

--- app/modules/file-management/storageManager.py
+++ app/modules/file-management/storageManager.py
@@ -6,16 +6,15 @@ class DataStorage:
     def __init__(self, base):
         # absolute path to project, storage directory, base_url + _time
         self.storagePath = os.path.join(builtins.absolute, 'storage', Helper.removeNonAlphaNumeric(base) + '_' + str(int(time.time())) )
-        self.storagePath = Helper.purgeWhitespace(self.storagePath)
         self.mkdir(self.storagePath)
 
     def saveItem(self, item):
         try:
             clonedItem = item.stringifyTags()
             # naming for this item. title__h1__unique
-            name = Helper.purgeWhitespace( str(clonedItem.id) + '__' + clonedItem.title + '__' + clonedItem.headerOne )
-            metaPath = os.path.join(self.storagePath, name + '.meta')
-            contentPath = os.path.join(self.storagePath, name + '.html')
+            name = str(clonedItem.id) + '__' + Helper.removeNonAlphaNumeric(clonedItem.title) + '__' + Helper.removeNonAlphaNumeric(clonedItem.headerOne)
+            metaPath = Helper.purgeWhitespace(os.path.join(self.storagePath, name + '.meta'))
+            contentPath = Helper.purgeWhitespace(os.path.join(self.storagePath, name + '.html'))
             self.write(contentPath, clonedItem.content)
             del clonedItem.content # we don't want content in our meta files
             self.write(metaPath, json.dumps(clonedItem.__dict__))
